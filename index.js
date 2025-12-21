dee3359dd5d32676b177ed721201f8ea6baa4996
lexwise-jr <jilalaalex@gmail.com>
2025-12-21 18:25:00 +0300

    Add start script for deployment

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const userRoutes = require('./Routes/user');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+     origin:['http://localhost:5173','http://localhost:3000'],
+     credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 // 4. Mount Routes
